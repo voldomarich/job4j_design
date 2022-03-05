d5fd369e7ee07160831379fda9721d421e57ca34
voldomarich <vbekasov1987@gmail.com>
2022-03-05 20:36:28 +0300

    0. Что такое обобщенные типы (generics) [#4952]

---
+++ src/main/java/ru/job4j/generics/Animal.java
@@ -0,0 +1,57 @@
+package ru.job4j.generics;
+
+import java.util.Date;
+import java.util.Objects;
+
+public class Animal {
+
+    private String name;
+
+    private int age;
+
+    private Date birthday;
+
+    public Animal(String name, int age, Date birthday) {
+        this.name = name;
+        this.age = age;
+        this.birthday = birthday;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public Date getBirthday() {
+        return birthday;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Animal animal = (Animal) o;
+        return Objects.equals(name, animal.name) && Objects.equals(birthday, animal.birthday);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, birthday);
+    }
+
+    @Override
+    public String toString() {
+        return "Animal{"
+                + "name='" + name + '\''
+                + ", age=" + age
+                + ", birthday=" + birthday
+                + '}';
+    }
+}
---
+++ src/main/java/ru/job4j/generics/Tiger.java
@@ -0,0 +1,9 @@
+package ru.job4j.generics;
+
+import java.util.Date;
+
+public class Tiger extends Predator {
+    public Tiger(String name, int age, Date birthday) {
+        super(name, age, birthday);
+    }
+}

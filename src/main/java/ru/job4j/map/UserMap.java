2c30db09e4f1b1b926e6813c9cc8400cb6a3e5f3
voldomarich <vbekasov1987@gmail.com>
2022-04-11 20:25:43 +0300

    2. Без переопределения equals и hashCode [#1005 #287355]

--- src/main/java/ru/job4j/map/UserMap.java
+++ src/main/java/ru/job4j/map/UserMap.java
@@ -6,16 +6,19 @@ import java.util.HashMap;
 
 public class UserMap {
 
-    HashMap<User, Object> map = new HashMap<>();
+    public static void main(String[] args) {
 
-    User user1 = new User("Igor", 0, new GregorianCalendar(2000, Calendar.APRIL, 11));
-    User user2 = new User("Igor", 0, new GregorianCalendar(2000, Calendar.APRIL, 11));
+        HashMap<User, Object> map = new HashMap<>();
 
-    map.put(user1, new Object());
-    map.put(user2, new Object());
+        User user1 = new User("Igor", 0, new GregorianCalendar(2000, Calendar.APRIL, 11));
+        User user2 = new User("Igor", 0, new GregorianCalendar(2000, Calendar.APRIL, 11));
 
-    for (User key : map.keySet()) {
-        Object value = map.get(key);
-        System.out.println(key + " " + value);
+        map.put(user1, new Object());
+        map.put(user2, new Object());
+
+        for (User key : map.keySet()) {
+            Object value = map.get(key);
+            System.out.println(key + " " + value);
+        }
     }
 }

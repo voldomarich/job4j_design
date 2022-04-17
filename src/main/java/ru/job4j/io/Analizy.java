cc9b31a07d3d998dc5706f3e3e77b23dfa8c0f37
voldomarich <vbekasov1987@gmail.com>
2022-04-17 22:27:49 +0300

    2. Анализ доступности сервера. [#859 #290012]

--- src/main/java/ru/job4j/io/Analizy.java
+++ src/main/java/ru/job4j/io/Analizy.java
@@ -1,38 +1,36 @@
 package ru.job4j.io;
 
 import java.io.*;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringJoiner;
 import java.util.stream.Collectors;
 
 public class Analizy {
 
-    private Map<String, String> values = new HashMap<>();
-
     public void unavailable(String source, String target) {
         try (BufferedReader in = new BufferedReader(new FileReader(source))) {
-            values = in.lines()
-                    .collect(Collectors.toMap((p) -> p.substring(0, 1), (p) -> p.substring(1, 2)))
-                    .entrySet()
-                    .stream()
-                    .filter(s -> s.getKey().equals("400") || s.getKey().equals("500"))
-                    .collect(Collectors.toMap(f -> f.getValue(), f -> f.getValue()));
+            a = in.lines()
+                    .filter(s -> s.startsWith("400") || s.startsWith("500"))
+                    .collect(Collectors.toList());
+            b = in.lines()
+                    .filter(s -> s.startsWith("200") || s.startsWith("300"))
+                    .collect(Collectors.toList());
+            target = a.split(" ")[0], b.split(" ")[1];
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
-    public static void save(Map<String, String> values, String target) {
-        try (PrintWriter out = new PrintWriter(
-                new BufferedOutputStream(
-                        new FileOutputStream(target)
-                ))) {
-            for (var line : values.entrySet()) {
-                out.println(line);
-            }
-        } catch (Exception e) {
+    @Override
+    public String toString() {
+        StringJoiner out = new StringJoiner(System.lineSeparator());
+        try (BufferedReader read = new BufferedReader(new FileReader(target))) {
+            read.lines().forEach(out::add);
+        } catch (IOException e) {
             e.printStackTrace();
         }
+        return out.toString();
     }
 
     public static void main(String[] args) {

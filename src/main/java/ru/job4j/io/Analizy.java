87b193cea12b502148d79700749a5d07e236798f
voldomarich <vbekasov1987@gmail.com>
2022-04-21 20:56:21 +0300

    2. Анализ доступности сервера. [#859 #290012]

--- src/main/java/ru/job4j/io/Analizy.java
+++ src/main/java/ru/job4j/io/Analizy.java
@@ -1,24 +1,27 @@
 package ru.job4j.io;
 
 import java.io.*;
-import java.util.Map;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.stream.Collectors;
 
 public class Analizy {
 
     public void unavailable(String source, String target) {
         try (BufferedReader in = new BufferedReader(new FileReader(source))) {
-            Map<String, String> a = in.lines()
-                    .filter(s -> s.startsWith("400") || s.startsWith("500"))
-                    .collect(Collectors.toMap(f -> f.split(" ")[0], f -> f.split(" ")[1]));
-            Map<String, String> b = in.lines()
-                    .filter(s -> s.startsWith("200") || s.startsWith("300"))
-                    .collect(Collectors.toMap(f -> f.split(" ")[0], f -> f.split(" ")[1]));
-            for (String key : a.keySet()) {
-                for (String k : b.keySet()) {
-                    target = a.get(key) + ";" + b.get(k);
+            List<String> result = in.lines()
+                    .collect(Collectors.toList());
+            StringBuilder rsl = new StringBuilder();
+            for (String i : result) {
+                if (i.startsWith("400") || i.startsWith(("500"))) {
+                    rsl.append(i.split(" ")[1] + ";");
                 }
+                if (i.startsWith("200") || i.startsWith("300")) {
+                    rsl.append(i.split(" ")[1]);
+                }
+                break;
             }
+            target = rsl.toString();
             try (PrintWriter out = new PrintWriter(
                     new BufferedOutputStream(
                             new FileOutputStream(target)
@@ -33,12 +36,7 @@ public class Analizy {
         }
     }
 
-    public static void main(String[] args) {
-        try (PrintWriter out = new PrintWriter(new FileOutputStream("unavailable.csv"))) {
-            out.println("15:01:30;15:02:32");
-            out.println("15:10:30;23:12:32");
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+    public void main(String[] args) {
+        unavailable("server.log", "unavailable.csv");
     }
 }

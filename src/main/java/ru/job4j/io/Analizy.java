c2b603a08ecb980d6d0d53c989c683b69856ae44
voldomarich <vbekasov1987@gmail.com>
2022-04-26 16:12:01 +0300

    2. Анализ доступности сервера. [#859 #290012]

--- src/main/java/ru/job4j/io/Analizy.java
+++ src/main/java/ru/job4j/io/Analizy.java
@@ -11,13 +11,16 @@ public class Analizy {
             List<String> result = in.lines()
                     .collect(Collectors.toList());
             StringBuilder rsl = new StringBuilder();
+            boolean status = true;
             for (String i : result) {
-                boolean memory = i.startsWith("400") || i.startsWith("500");
-                if (memory) {
-                    rsl.append(i.split(" ")[1]).append(";");
-                } else if (i.startsWith("200") || i.startsWith("300")) {
-                    rsl.append(i.split(" ")[1]);
-                }
+                if ((i.startsWith("400") || i.startsWith("500"))
+                        && !i.equals(result.get(result.size() - 1)) && status) {
+                    status = false;
+                    rsl.append(i.split(" ")[1]).append("; ");
+                } else if ((i.startsWith("200") || i.startsWith("300")) && !status) {
+                    status = true;
+                    rsl.append(i.split(" ")[1]).append((System.lineSeparator()));
+                    }
             }
             try (PrintWriter out = new PrintWriter(
                     new BufferedOutputStream(

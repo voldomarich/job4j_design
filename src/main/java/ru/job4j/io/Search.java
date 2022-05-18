ef55e53df623b6e6c2502c1f70c515538a52493c
voldomarich <vbekasov1987@gmail.com>
2022-05-18 19:09:45 +0300

    4.1. Сканирование файловой системы. [#106929 #302150]

--- src/main/java/ru/job4j/io/Search.java
+++ src/main/java/ru/job4j/io/Search.java
@@ -8,6 +8,7 @@ import java.util.List;
 import java.util.function.Predicate;
 
 public class Search {
+
     public static void main(String[] args) throws IOException {
         Path start = Paths.get(".");
         search(start, p -> p.toFile().getName().endsWith(".js")).forEach(System.out::println);
--- src/main/java/ru/job4j/io/SearchFiles.java
+++ src/main/java/ru/job4j/io/SearchFiles.java
@@ -3,6 +3,7 @@ package ru.job4j.io;
 import java.io.IOException;
 import java.nio.file.*;
 import java.nio.file.attribute.BasicFileAttributes;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Predicate;
 
@@ -11,7 +12,7 @@ import static java.nio.file.FileVisitResult.CONTINUE;
 public class SearchFiles implements FileVisitor<Path> {
 
     Predicate<Path> condition;
-    List<Path> result;
+    List<Path> result = new ArrayList<>();
 
     public SearchFiles(Predicate<Path> condition) {
         this.condition = condition;
@@ -24,12 +25,9 @@ public class SearchFiles implements FileVisitor<Path> {
 
     @Override
     public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
-        List<String> lines = Files.readAllLines(file);
-        for (String t: lines) {
-            if (condition.test(Path.of(t))) {
-                Path path = file.toAbsolutePath();
-                this.result.add(path);
-            }
+        if (condition.test(file)) {
+            Path path = file.toAbsolutePath();
+            result.add(path);
         }
         return FileVisitResult.CONTINUE;
     }

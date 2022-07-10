a2d3d76355c9f085ae40ee72c8444b94fed6209f
voldomarich <vbekasov1987@gmail.com>
2022-07-10 21:51:06 +0300

    5.2. Архивировать проект [#861 #309002]

--- src/main/java/ru/job4j/io/Zip.java
+++ src/main/java/ru/job4j/io/Zip.java
@@ -9,37 +9,22 @@ import java.util.zip.ZipOutputStream;
 
 public class Zip {
 
-    List<Path> result = new LinkedList<>();
-
-    public void packFiles(List<File> sources, File target) throws IOException {
+    public void packFiles(List<Path> result, File target) {
         try (ZipOutputStream zip = new ZipOutputStream(new BufferedOutputStream(
                 new FileOutputStream(target)))) {
             for (Path path : result) {
-                for (File source : sources) {
-                    zip.putNextEntry(new ZipEntry((ZipEntry) path));
-                    try (BufferedInputStream out = new BufferedInputStream(new FileInputStream(source))) {
+                    zip.putNextEntry((ZipEntry) path);
+                    try (BufferedInputStream out = new BufferedInputStream(new FileInputStream(String.valueOf(path)))) {
                         zip.write(out.readAllBytes());
                     }
                 }
-            }
             } catch (Exception e) {
                 e.printStackTrace();
             }
         }
 
-    public void packSingleFile(File source, File target) {
-        try (ZipOutputStream zip = new ZipOutputStream(new BufferedOutputStream(
-                new FileOutputStream(target)))) {
-            zip.putNextEntry(new ZipEntry(source.getPath()));
-            try (BufferedInputStream out = new BufferedInputStream(new FileInputStream(source))) {
-                zip.write(out.readAllBytes());
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
     public static void main(String[] args) throws IOException {
+        List<Path> result = new LinkedList<>();
         if (args.length == 0) {
             throw new IllegalArgumentException("Корневая папка пуста "
                     + "Usage java -jar argsname.jar ROOT_FOLDER"
@@ -48,10 +33,5 @@ public class Zip {
         ArgsName arguments = ArgsName.of(args);
         result.addAll(Search.search(Path.of(arguments.get("d")),
                 p -> !p.toFile().getName().endsWith(arguments.get("e"))));
-        Zip zip = new Zip();
-        zip.packSingleFile(
-                new File(arguments.get("d")),
-                new File(arguments.get("o"))
-        );
     }
 }

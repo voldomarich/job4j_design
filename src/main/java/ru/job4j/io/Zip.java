26653dc8d7308e824ed44b794ae29adc6dab83d5
voldomarich <vbekasov1987@gmail.com>
2022-07-12 10:59:22 +0300

    5.2. Архивировать проект [#861 #309002]

--- src/main/java/ru/job4j/io/Zip.java
+++ src/main/java/ru/job4j/io/Zip.java
@@ -23,8 +23,8 @@ public class Zip {
         }
     }
 
-    public static void main(String[] args) throws IOException {
-        if (args.length == 0) {
+    private static boolean validation(String[] args) {
+        if (args.length != 3) {
             throw new IllegalArgumentException("Корневая папка пуста "
                     + "Usage java -jar argsname.jar ROOT_FOLDER"
             );
@@ -37,10 +37,17 @@ public class Zip {
         if (!file.isDirectory()) {
             throw new IllegalArgumentException(String.format("Not directory %s", file.getAbsoluteFile()));
         }
-        List<Path> result = new LinkedList<>(Search.search(Path.of(arguments.get("d")),
-                p -> !p.toFile().getName().endsWith(arguments.get("e"))));
-        File target = new File(arguments.get("o"));
-        Zip zip = new Zip();
-        zip.packFiles(result, target);
+        return true;
+    }
+
+    public static void main(String[] args) throws IOException {
+        if (validation(args)) {
+            ArgsName arguments = ArgsName.of(args);
+            List<Path> result = new LinkedList<>(Search.search(Path.of(arguments.get("d")),
+                    p -> !p.toFile().getName().endsWith(arguments.get("e"))));
+            File target = new File(arguments.get("o"));
+            Zip zip = new Zip();
+            zip.packFiles(result, target);
+        }
     }
 }

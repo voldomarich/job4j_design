fd26e21fd11de955026555e41c25eb3e69496aa2
voldomarich <vbekasov1987@gmail.com>
2022-09-07 15:59:10 +0300

    3. Slf4j - вывод переменных. [#268852]

--- src/main/java/ru/job4j/io/UsageLog4j.java
+++ src/main/java/ru/job4j/io/UsageLog4j.java
@@ -8,10 +8,18 @@ public class UsageLog4j {
     private static final Logger LOG = LoggerFactory.getLogger(UsageLog4j.class.getName());
 
     public static void main(String[] args) {
-        LOG.trace("trace message");
-        LOG.debug("debug message");
-        LOG.info("info message");
-        LOG.warn("warn message");
-        LOG.error("error message");
+        String name = "Vladimir";
+        byte age = 34;
+        short temperature = -15;
+        int sum = 2_142_500;
+        long earthPopulation = 2_100_115_000;
+        float cellCount = 3.72e13f;
+        double numberOfMosquitos = 1.92E11;
+        char dayOfWeek = '3';
+        boolean status = true;
+        LOG.debug("User info name : {}, age : {}, temperature : {}, sum : {}, earthPopulation : {}, cellCount : {},"
+                + "numberOfMosquitos : {}, dayOfWeek : {}, status : {}",
+                name, age, temperature, sum, earthPopulation, cellCount,
+                numberOfMosquitos, dayOfWeek, status);
     }
 }

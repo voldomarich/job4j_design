93da6a7e06bf795dec877fdbee24b14870e2f6c4
voldomarich <vbekasov1987@gmail.com>
2022-09-18 16:53:14 +0300

    6. Кодировка. [#862 #338114]

--- src/main/java/ru/job4j/io/ConsoleChat.java
+++ src/main/java/ru/job4j/io/ConsoleChat.java
@@ -20,57 +20,25 @@ public class ConsoleChat {
     }
 
     public void run() {
-        saveLog(readPhrases());
+        List<String> list = readPhrases();
+        saveLog(list);
     }
 
     private List<String> readPhrases() {
-        boolean status = true;
         List<String> result = new ArrayList<>();
-        StringBuilder builder = new StringBuilder();
-        Scanner scanner = new Scanner(System.in);
-        String userChat = "";
-        writeDataInFile(userChat, scanner.toString());
-
-        if (readFile(userChat).contains(STOP) && status) {
-            run();
-            status = false;
-
-        } else if (readFile(userChat).contains(CONTINUE) && !status) {
-            try (BufferedReader b = new BufferedReader(new FileReader(botAnswers))) {
-                b.lines().map(s -> s + System.lineSeparator()).forEach(builder::append);
-                status = true;
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        } else if (readFile(userChat).contains(OUT) && status) {
-            status = false;
-        }
-        result.add(builder.toString());
-        result.add(userChat);
-        return result;
-    }
-
-    private void saveLog(List<String> log) {
-        try (PrintWriter pw = new PrintWriter(new FileWriter(path, Charset.forName("WINDOWS-1251"), true))) {
-            log.forEach(pw::println);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    private String readFile(String path) {
         StringBuilder builder = new StringBuilder();
         try (BufferedReader br = new BufferedReader(new FileReader(path))) {
             br.lines().map(s -> s + System.lineSeparator()).forEach(builder::append);
         } catch (IOException e) {
             e.printStackTrace();
         }
-        return builder.toString();
+        result.add(builder.toString());
+        return result;
     }
 
-    private void writeDataInFile(String path, String data) {
-        try (PrintWriter pw = new PrintWriter(new FileWriter(path, true))) {
-            pw.println(data);
+    private void saveLog(List<String> log) {
+        try (PrintWriter pw = new PrintWriter(new FileWriter(botAnswers, Charset.forName("WINDOWS-1251"), true))) {
+            log.forEach(pw::println);
         } catch (IOException e) {
             e.printStackTrace();
         }

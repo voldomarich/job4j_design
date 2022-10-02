cf87e72338f2d61a62a2a3d70b58a050a47dfb7a
voldomarich <vbekasov1987@gmail.com>
2022-10-03 00:36:41 +0300

    0.1. Statement [#379306]

--- src/main/java/ru/job4j/jdbc/TableEditor.java
+++ src/main/java/ru/job4j/jdbc/TableEditor.java
@@ -1,31 +1,79 @@
 package ru.job4j.jdbc;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Properties;
 import java.util.StringJoiner;
 
 public class TableEditor implements AutoCloseable {
 
     private Connection connection;
 
+    private Properties properties;
+
+    public TableEditor(Properties properties) {
+        this.properties = properties;
+        initConnection();
+    }
+
     private void initConnection() {
         connection = null;
     }
 
-    public void createTable(String tableName) {
+    public void createTable(String tableName) throws Exception {
+        try (Connection connection = DriverManager.getConnection("url", "login", "password")) {
+            try (Statement statement = connection.createStatement()) {
+                statement.execute(tableName);
+            }
+        }
     }
 
-    public void dropTable(String tableName) {
+    public static void dropTable(String tableName) throws Exception {
+        try (Connection connection = DriverManager.getConnection("url", "login", "password")) {
+            try (Statement statement = connection.createStatement()) {
+                statement.executeUpdate(tableName);
+                System.out.println("Table is deleted");
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
-    public void addColumn(String tableName, String columnName, String type) {
+    public void addColumn(String tableName, String columnName, String type) throws Exception {
+        try (Connection connection = DriverManager.getConnection("url", "login", "password")) {
+            try (Statement statement = connection.createStatement()) {
+                int column = statement.executeUpdate("Add FROM tableName WHERE Id = columnName");
+                System.out.printf("%d column(s) added", column);
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
-    public void dropColumn(String tableName, String columnName) {
+    public void dropColumn(String tableName, String columnName) throws Exception {
+        try (Connection connection = DriverManager.getConnection("url", "login", "password")) {
+            try (Statement statement = connection.createStatement()) {
+                int column = statement.executeUpdate("DELETE FROM tableName WHERE Id = columnName");
+                System.out.printf("%d column(s) deleted", column);
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
-    public void renameColumn(String tableName, String columnName, String newColumnName) {
+    public void renameColumn(String tableName, String columnName, String newColumnName) throws Exception {
+        try (Connection connection = DriverManager.getConnection("url", "login", "password")) {
+            try (Statement statement = connection.createStatement()) {
+                int column = statement.executeUpdate("UPDATE tableName SET columnName = newColumnName");
+                System.out.printf("%d column(s) renamed", column);
+            } catch (Exception e) {
+                System.out.println("Connection failed...");
+                System.out.println(e);
+            }
+        }
     }
-
 
     public static String getTableScheme(Connection connection, String tableName) throws Exception {
         var rowSeparator = "-".repeat(30).concat(System.lineSeparator());
@@ -51,5 +99,9 @@ public class TableEditor implements AutoCloseable {
         if (connection != null) {
             connection.close();
         }
+    }
+
+    public static void main(String[] args) throws Exception {
+        System.out.println();
     }
 }

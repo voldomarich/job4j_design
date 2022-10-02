2fe614c334e8ba3643b083b1efb1f814cb45fc05
voldomarich <vbekasov1987@gmail.com>
2022-10-02 17:49:58 +0300

    4. Slf4j - вывод exception. [#268853 #347917]

--- src/main/java/ru/job4j/io/EchoServer.java
+++ src/main/java/ru/job4j/io/EchoServer.java
@@ -11,7 +11,7 @@ import java.net.Socket;
 
 public class EchoServer {
 
-    private static final Logger LOG = LoggerFactory.getLogger(UsageLog4j.class.getName());
+    private static final Logger LOG = LoggerFactory.getLogger(EchoServer.class.getName());
 
     public static void main(String[] args) {
         try (ServerSocket server = new ServerSocket(9000)) {
@@ -21,14 +21,19 @@ public class EchoServer {
                      BufferedReader in = new BufferedReader(
                              new InputStreamReader(socket.getInputStream()))) {
                     out.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
-                    for (String str = in.readLine(); str != null && !str.isEmpty(); str = in.readLine()) {
-                        if (str.contains("Exit")) {
-                            server.close();
-                        } else if (str.contains("Hello")) {
-                            out.write("Hello".getBytes());
-                        } else {
-                            out.write("What".getBytes());
-                        }
+                    String string = in.readLine();
+                    if (string.contains("Exit")) {
+                        server.close();
+                        System.out.println("Server is successfully closed");
+                        break;
+                    } else if (string.contains("Hello")) {
+                        out.write("Hello".getBytes());
+                        System.out.println(string);
+                        System.out.println("Hello");
+                    } else {
+                        out.write("What".getBytes());
+                        System.out.println(string);
+                        System.out.println("What");
                     }
                     out.flush();
                 }

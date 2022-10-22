b405ab4696c8dc2ec5367994670a9615a01a1fa1
voldomarich <vbekasov1987@gmail.com>
2022-10-22 16:10:48 +0300

    0.1. Statement [#379306 #348148]

--- src/main/java/ru/job4j/jdbc/TableEditor.java
+++ src/main/java/ru/job4j/jdbc/TableEditor.java
@@ -26,30 +26,29 @@ public class TableEditor implements AutoCloseable {
 
     public void createTable(String tableName) throws Exception {
         String sql = String.format("CREATE table %s (id serial primary key);", tableName);
-        System.out.println(getTableScheme(connection, "car"));
+        System.out.println(getTableScheme(tableName));
     }
 
     public void dropTable(String tableName) throws Exception {
         String sql = String.format("DROP table %s;", tableName);
-        System.out.println(getTableScheme(connection, "car"));
     }
 
     public void addColumn(String tableName, String columnName, String type) throws Exception {
         String sql = String.format("ALTER table %s ADD %s %s NULL;", tableName, columnName, type);
-        System.out.println(getTableScheme(connection, "car"));
+        System.out.println(getTableScheme(tableName));
     }
 
     public void dropColumn(String tableName, String columnName) throws Exception {
         String sql = String.format("ALTER table %s DROP COLUMN %s;", tableName, columnName);
-        System.out.println(getTableScheme(connection, "car"));
+        System.out.println(getTableScheme(tableName));
     }
 
     public void renameColumn(String tableName, String columnName, String newColumnName) throws Exception {
         String sql = String.format("ALTER table %s RENAME COLUMN %s TO %s;", tableName, columnName, newColumnName);
-        System.out.println(getTableScheme(connection, "car"));
+        System.out.println(getTableScheme(tableName));
     }
 
-    public static String getTableScheme(Connection connection, String tableName) throws Exception {
+    public String getTableScheme(String tableName) throws Exception {
         var rowSeparator = "-".repeat(30).concat(System.lineSeparator());
         var header = String.format("%-15s|%-15s%n", "NAME", "TYPE");
         var buffer = new StringJoiner(rowSeparator, rowSeparator, rowSeparator);
@@ -87,11 +86,9 @@ public class TableEditor implements AutoCloseable {
                 tableEditor.addColumn("car", "transmission", "text");
                 tableEditor.dropColumn("car", "transmission");
                 tableEditor.renameColumn("car", "bodies", "body");
-                tableEditor.dropTable("car");
             } catch (SQLException e) {
                 e.printStackTrace();
             }
-
         }
     }
 }

b40c00777fd66cd8f5ac3585ab41339ba85c4ad1
voldomarich <vbekasov1987@gmail.com>
2022-10-29 00:51:38 +0300

    7. Scanner [#504791 #347683]

--- src/main/java/ru/job4j/io/CSVReader.java
+++ src/main/java/ru/job4j/io/CSVReader.java
@@ -7,7 +7,25 @@ import java.util.List;
 public class CSVReader {
 
     public static void handle(ArgsName argsName) throws Exception {
+        private boolean validation(String[] args) {
+            if (ArgsName.of(args) != 4) {
+                throw new IllegalArgumentException("Root folder is supposed to have four arguments. "
+                        + "Usage java -jar search.jar ROOT_FOLDER .JS"
+                );
+            }
+            if (!argsName.get("path").endsWith(".csv")) {
+                throw new IllegalArgumentException("Root folder is supposed to have argument of format .csv "
+                        + "Usage java -jar search.jar ROOT_FOLDER .JS");
+            }
+            return true;
+        }
+        if (args.length == 0) {
+            throw new IllegalArgumentException("Корневая папка пуста "
+                    + "Usage java -jar argsname.jar ROOT_FOLDER"
+            );
+        }
         List<String> result = new ArrayList<>();
+        result.add(argsName.get("filter").replace(",", ";"));
         StringBuilder rsl = new StringBuilder();
         String[] names = argsName.get("filter").split(",");
         try (BufferedReader in = new BufferedReader(new FileReader(argsName.get("path")))) {
@@ -25,11 +43,12 @@ public class CSVReader {
             while (in.ready()) {
                 line = in.readLine();
                 String[] str = line.split(argsName.get("delimiter"));
-                rsl.append(argsName.get("filter")).append(argsName.get("delimiter"));
                 for (int index : indexes) {
                     rsl.append(str[index]).append(argsName.get("delimiter"));
                 }
+                rsl.deleteCharAt(rsl.length() - 1);
                 result.add(rsl.toString());
+                rsl.delete(0, rsl.length());
             }
         }
         try (PrintWriter out = new PrintWriter(
@@ -42,27 +61,10 @@ public class CSVReader {
         }
     }
 
-    private static boolean validation(String[] args) {
-        ArgsName argsName = ArgsName.of(args);
-        if (args.length != 4) {
-            throw new IllegalArgumentException("Root folder is supposed to have four arguments. "
-                    + "Usage java -jar search.jar ROOT_FOLDER .JS"
-            );
-        }
-        if (!argsName.get("path").endsWith(".csv")) {
-            throw new IllegalArgumentException("Root folder is supposed to have argument of format .csv "
-                    + "Usage java -jar search.jar ROOT_FOLDER .JS");
-        }
-        return true;
-    }
+
 
     public static void main(String[] args) throws Exception {
         ArgsName argsName = ArgsName.of(args);
-        if (args.length == 0) {
-            throw new IllegalArgumentException("Корневая папка пуста "
-                    + "Usage java -jar argsname.jar ROOT_FOLDER"
-            );
-        }
         if (validation(args)) {
             handle(argsName);
         }

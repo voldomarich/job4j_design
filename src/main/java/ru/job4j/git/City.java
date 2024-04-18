44f7577785c5a3dffc29149e5bab500b7a464cab
voldomarich <vbekasov1987@gmail.com>
2024-04-18 10:02:02 +0300

    0. Принцип инверсии зависимостей [#4917]

---
+++ src/main/java/ru/job4j/git/City.java
@@ -0,0 +1,54 @@
+package ru.job4j.git;
+
+import java.util.Date;
+import java.util.Objects;
+
+public class City {
+
+    private String name;
+
+    private int age;
+
+    private Date birthday;
+
+    public City() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public Date getBirthday() {
+        return birthday;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        City city = (City) o;
+        return Objects.equals(name, city.name) && Objects.equals(birthday, city.birthday);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, birthday);
+    }
+
+    @Override
+    public String toString() {
+        return "Animal{"
+                + "name='" + name + '\''
+                + ", age=" + age
+                + ", birthday=" + birthday
+                + '}';
+    }
+}
